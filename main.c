b0a23a415cdb33e3eab0d6a7355229571d9bbbfa
aherzogenrath-io <aherzogenrath@infonline.de>
2024-10-12 15:21:31 +0200

    Input messages

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+In this programm there will be a user input of any number and its number system (bin, hex, dec)
+The user choses a number system and it will be converted into the chosen system
+*/
+
+
+int main(void)
+{
+    //User Chooses a number system
+    printf("Choose a number system for your input\n");
+    printf("Input:\t\t\"b\" for binary \t\t\"d\" for decimal\t\t\"h\" for hexadecimal\n");
+
+    return 0;
+}
